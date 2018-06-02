4030208d1fdfdf03ef25fe14590b7de42ba9ea8d
deusyvan <deusyvan@gmail.com>
2018-06-02 14:56:09 -0300

    Formulário de login

---
+++ login.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Caixa Eletrônico</title>
+</head>
+<body>
+	<form method="POST">
+		Agência:<br/>
+		<input type="text" name="agencia" /><br/><br/>
+		
+		Conta:<br/>
+		<input type="text" name="conta" /><br/><br/>
+		
+		Senha:<br/>
+		<input type="password" name="senha" /><br/><br/>
+		
+		<input type="submit" value="Entrar" /><br/><br/>
+	</form>
+</body>
+</html>
