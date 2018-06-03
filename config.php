522fe99097957e14d926ee8b10498f8728cace8b
deusyvan <deusyvan@gmail.com>
2018-06-03 13:09:36 -0300

    Mostrar erros

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 try {
     $pdo = new PDO("mysql:dbname=caixa;host=localhost", "admin", "admin");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "ERRO: ".$e->getMessage();
     exit;
