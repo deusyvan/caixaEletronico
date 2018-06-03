7724a45f8973ecd6db2a290858b8fa53ef023b22
deusyvan <deusyvan@gmail.com>
2018-06-03 17:57:57 -0300

    Realizando a transação com os dados do formulario e retornando ao index

--- add-transacao.php
+++ add-transacao.php
@@ -2,10 +2,35 @@
 session_start();
 require 'config.php';
 
-if (isset($_POST['tipo']) && empty($_POST['tipo']) == FALSE) {
+if (isset($_POST['tipo'])) {
     $tipo = $_POST['tipo'];
-    $valor = $_POST['valor'];
+    $valor = str_replace(",", ".", $_POST['valor']);
+    $valor = floatval($valor);
     
+    $sql = $pdo->prepare("INSERT INTO historico (id_conta, tipo, valor, data_operacao) 
+                          VALUES (:id_conta, :tipo, :valor, NOW())");
+    $sql->bindValue(":id_conta", $_SESSION['banco']);
+    $sql->bindValue(":tipo", $tipo);
+    $sql->bindValue(":valor", $valor);
+    $sql->execute();
+    
+    if ($tipo == '0') {
+        //Depósito
+        $sql = $pdo->prepare("UPDATE contas SET saldo = saldo + :valor WHERE id = :id");
+        $sql->bindValue(":valor", $valor);
+        $sql->bindValue(":id", $_SESSION['banco']);
+        $sql->execute();
+        
+    } else {
+        //Saque
+        $sql = $pdo->prepare("UPDATE contas SET saldo = saldo - :valor WHERE id = :id");
+        $sql->bindValue(":valor", $valor);
+        $sql->bindValue(":id", $_SESSION['banco']);
+        $sql->execute();
+    }
+    
+    header("Location: index.php");
+    exit();
     
 }
 ?>
