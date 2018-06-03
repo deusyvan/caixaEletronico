93c580117b4239d445fed6a98114ad32d1dac1e0
deusyvan <deusyvan@gmail.com>
2018-06-03 11:10:45 -0300

    Importamos o config e realizamos consulta no banco para comparar com os dados obtidos do formulário

--- login.php
+++ login.php
@@ -1,11 +1,22 @@
 <?php 
 session_start();
+require 'config.php';
 
 if(isset($_SESSION['agencia']) && empty($_SESSION['agencia']) == FALSE){
     $agencia = addslashes($_POST['agencia']);
     $conta = addslashes($_POST['conta']);
-    $senha = addslashes($_POST['senha']);
+    $senha = md5(addslashes($_POST['senha']));
+    
+    $sql = $pdo->prepare("SELECT * FROM contas WHERE agencia = :agencia AND conta = :conta AND senha = :senha");
+    $sql->bindValue(":agencia", $agencia);
+    $sql->bindValue(":conta", $conta);
+    $sql->bindValue(":senha", @senha);
+    $sql->execute();
+    
+    
+    
 } 
+
 
 ?>
 <html>
